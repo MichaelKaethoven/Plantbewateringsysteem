c05484873b4fb122bdcc24b620eebbe1c3dc1600
Michael Kaethoven <michael.kaethoven@gmail.com>
2026-02-16 11:53:54 +0100

    Init commit

---
+++ Implementatie/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+  BRONNEN:
+  Canvas cursus - 16/02/2026 - https://canvas.kdg.be/courses/55542
+
+*/
+
+#include <Arduino.h>
+#include <config.h>
+#include <functionDeclarations.h>
+
+void setup() {}
+
+void loop() {}
